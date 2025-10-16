c3bea096d1561cb35f3c7021e828dbb322637bc5
Johannes Rödel <johannesroedelportfolio@gmail.com>
2025-10-16 03:03:49 +0200

    Updated gh deploy scipt

--- simple-blog-gh.sh
+++ simple-blog-gh.sh
@@ -2,8 +2,15 @@
 python -m venv venv
 source venv/bin/activate
 
-# Making sure the right package is installed
-./simple-blog-install-from-tar.sh
+# Update pip
+pip install --upgrade pip
+
+# Uninstall old versions if any
+pip uninstall mkdocs mkdocs-simple-blog -y
+
+# Install required packages
+pip install mkdocs --no-cache-dir
+pip install dist/mkdocs_simple_blog-0.2.0.tar.gz --no-cache-dir
 
 # Build and deploy documentation to GitHub Pages
 mkdocs build
